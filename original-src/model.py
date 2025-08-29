d7a39d51f74450a3e622dc52c9506c8f0f83fbd3
spideynolove <spideynolove@gmail.com>
2025-08-29 23:52:29 +0700

    delete all shitty content, recreate this project from scratch

--- original-src/model.py
+++ original-src/model.py
@@ -5,7 +5,7 @@ import torch.nn.functional as F
 import os
 
 class Linear_QNet(nn.Module):
-    def __init__(self, input_size=4, hidden_size=256, output_size=3):
+    def __init__(self, input_size, hidden_size, output_size):
         super().__init__()
         self.linear1 = nn.Linear(input_size, hidden_size)
         self.linear2 = nn.Linear(hidden_size, output_size)
@@ -23,14 +23,9 @@ class Linear_QNet(nn.Module):
         file_name = os.path.join(model_folder_path, file_name)
         torch.save(self.state_dict(), file_name)
 
-    def load(self, file_name='model.pth'):
-        model_folder_path = './model'
-        file_name = os.path.join(model_folder_path, file_name)
-        if os.path.exists(file_name):
-            self.load_state_dict(torch.load(file_name))
 
 class QTrainer:
-    def __init__(self, model, lr=0.001, gamma=0.9):
+    def __init__(self, model, lr, gamma):
         self.lr = lr
         self.gamma = gamma
         self.model = model
@@ -42,14 +37,17 @@ class QTrainer:
         next_state = torch.tensor(next_state, dtype=torch.float)
         action = torch.tensor(action, dtype=torch.long)
         reward = torch.tensor(reward, dtype=torch.float)
+        # (n, x)
 
         if len(state.shape) == 1:
+            # (1, x)
             state = torch.unsqueeze(state, 0)
             next_state = torch.unsqueeze(next_state, 0)
             action = torch.unsqueeze(action, 0)
             reward = torch.unsqueeze(reward, 0)
             done = (done, )
 
+        # 1: predicted Q values with current state
         pred = self.model(state)
 
         target = pred.clone()
@@ -60,7 +58,11 @@ class QTrainer:
 
             target[idx][torch.argmax(action[idx]).item()] = Q_new
     
+        # 2: Q_new = r + y * max(next_predicted Q value) -> only do this if not done
+        # pred.clone()
+        # preds[argmax(action)] = Q_new
         self.optimizer.zero_grad()
         loss = self.criterion(target, pred)
         loss.backward()
+
         self.optimizer.step()
